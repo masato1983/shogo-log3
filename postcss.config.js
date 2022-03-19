af1c38eaed48c8fa744cfe480776a291e41cf798
masato1983 <50264750+masato1983@users.noreply.github.com>
2022-03-19 09:53:35 +0900

    build(postcss.config.js): Update plugins

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
     plugins: [
         require('postcss-gap-properties'),
         require('postcss-clip-path-polyfill'),
-        require('postcss-viewport-height-correction'),
-        require('postcss-100vh-fix'),
+        // require('postcss-viewport-height-correction'),
+        // require('postcss-100vh-fix'),
         require('postcss-flexbugs-fixes'),
         require('postcss-rem'),
         require('autoprefixer')({ grid: 'autoplace' }),
